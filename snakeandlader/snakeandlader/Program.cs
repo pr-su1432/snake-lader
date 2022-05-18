1c6f0c71e880a8a591f5cdcec4b4c92a7d47bf08
prasanna lakshmi <prasannalakshmikathi@gmail.com>
2022-05-18 19:52:11 +0530

    [prasanna]: checkstatus position zero

---
+++ snakeandlader/snakeandlader/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace snakeandlader
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Snakeandlader.Snakeandlader value = new Snakeandlader.Snakeandlader();
+            value.Position();
+        }
+    }
+
+}
---
+++ snakeandlader/snakeandlader/snakeandlader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snakeandlader
+{
+    internal class Snakeandlader
+    {
+
+        int startpositon = 0, playerposition = 0;
+        public void Position()
+        {
+            Console.WriteLine("Initial  position of player is " + playerposition);
+
+        }
+
+
+    }
+}
+
+
+
